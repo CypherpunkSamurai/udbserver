7c0fb18e5a6f1571c6d535ccd05d5b8ec7a4aaf7
Cypherpunk Samurai <66906402+CypherpunkSamurai@users.noreply.github.com>
2022-06-28 02:49:41 +0530

    Update setup.py

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -2,25 +2,12 @@
 # encoding: utf-8
 
 from setuptools import setup, Extension
-import os
-
-build_dir = "../../build/"
-c_headers = []
-
-
-if not os.listdir(build_dir) == []:
-    _builds = os.listdir(build_dir)
-    header_file = "/include/udbserver.h"
-    c_headers = [build_dir + x + header_file for x in _builds]
-elif os.path.exists("udbserver.h"):
-    c_headers.append("./")
-else:
-    raise Exception("No header files found...")
-
 
 rust_module = Extension('udbserver',
                            sources=['udbserver.c'],
                            libraries=['udbserver'],
+                           library_dirs=['../../build/win/lib/'],
+                           include_dirs=['../../build/win/include/'],
                            )
 
 setup (name = 'udbserver',
@@ -38,5 +25,4 @@ setup (name = 'udbserver',
        ],
        ext_modules = [rust_module],
        py_modules = [],
-       headers=c_headers,
        )
